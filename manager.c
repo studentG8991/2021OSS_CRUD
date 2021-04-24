290674bbb086b046916613e0904b50af3a76ba7c
HurSeungDon <zzaa9999@naver.com>
2021-04-24 16:56:12 +0900

    modified manager.c

--- manager.c
+++ manager.c
@@ -40,7 +40,17 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
+	if(fp == NULL) 
+	{
+		printf("파일 열기  실패\n");
+		return;
+	}
+
+	for(int i = 0 ; i < count ; i++)
+	{
+		if(p[i].weight == -1) continue;
+		fprintf(fp,"%d %d %s\n",p[i].weight,p[i].price,p[i].name);
+	}	
 	
 	fclose(fp);
 	printf("저장됨!\n");
@@ -53,10 +63,18 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
-
-
-
+	fp = fopen("product.txt","rt");
+	if(fp == NULL)
+	{
+		printf("파일 열기 실패\n");
+		return 0;
+	}
+	while(!feof(fp))
+	{
+		fscanf(fp,"%d %d %s\n",&p[count].weight,&p[count].price,p[count].name);
+		count++;
+	}
+	fclose(fp);
 	printf("=> 로딩 성공!\n");
 	return count;
 }
